ab4865bf0e48a02e77b5f438356595d773b47859
mbero_akoko <mberoakoko@gmail.com>
2025-06-04 22:40:15 +0200

    Figuring out the ins and outs of the visitior pattern

--- src/sims/one_factor_monte_carlo.hpp
+++ src/sims/one_factor_monte_carlo.hpp
@@ -5,6 +5,8 @@
 #ifndef ONE_FACTOR_MONTE_CARLO_HPP
 #define ONE_FACTOR_MONTE_CARLO_HPP
 #include <bits/stdc++.h>
+#include <pstl/execution_impl.h>
+
 namespace OneFactorMonteCarlo {
     template<typename T>
     struct  Range {
@@ -66,5 +68,65 @@ namespace OneFactorMonteCarlo {
         drift drift_;
         diffusion diffusion_;
     };
+
+    template<typename T, typename R>
+    using Vector = std::vector<T>;;
+
+    class OneFactorSDEVisitor {
+    public:
+        explicit OneFactorSDEVisitor() = default;
+        explicit OneFactorSDEVisitor(const OneFactorSDEVisitor& visitor) = default;
+        virtual ~OneFactorSDEVisitor() = default;
+
+        virtual auto visit(const SDETypeD& sde_type_d) -> void  = 0;
+        // TODO: other SDE types must be implemented
+
+        auto operator()(const OneFactorSDEVisitor& source) -> OneFactorSDEVisitor& {
+            return *this;
+        };
+    };
+
+    class FiniteDifferenceMethodsVisitor : public OneFactorSDEVisitor {
+    public:
+        explicit FiniteDifferenceMethodsVisitor() = default;
+        explicit FiniteDifferenceMethodsVisitor(const std::size_t& N , const double dt,
+            const double initial_value )
+        :   initial_condition(initial_value), dt_(dt), num_steps(N){}
+
+        auto set_random_vector(const std::vector<double>& random_vector) -> void {
+            std::ranges::copy(random_vector, std::back_inserter(d_W));
+        }
+
+        auto get_path() -> std::vector<double> {
+            return result;
+        }
+
+    protected:
+        double initial_condition, v_previous {0};
+        double v_current {0};
+        double dt_;
+        std::size_t num_steps;
+        // TODO: Datastrucures to store result, path and state matrices
+        std::vector<double> result;
+        std::vector<double> d_W;
+    };
+
+    class ExplicitEuler final: FiniteDifferenceMethodsVisitor {
+    public:
+        explicit ExplicitEuler() = default;
+        explicit ExplicitEuler(const std::size_t& N , const double dt,const double initial_value )
+        : FiniteDifferenceMethodsVisitor(N,dt,initial_value){};
+
+        auto visit(const SDETypeD &sde_type_d) -> void override {
+            v_previous = initial_condition;
+            result.push_back(initial_condition);
+            auto euler_step = [&](std::size_t index) -> double {
+                v_current = v_previous * sde_type_d.calculate_drift()
+                v_previous = v_current;
+            }(1);
+        };
+
+    };
+
 }
 #endif //ONE_FACTOR_MONTE_CARLO_HPP
